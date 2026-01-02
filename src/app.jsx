b8de9f87d1504c81e720cca186b8fd905c25e4cb
Henry-Paul <164559529+Henry-Paul@users.noreply.github.com>
2026-01-02 21:45:37 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
 import Offerings from "./pages/Offerings";
+import Blog from "./pages/Blog";
+import BlogDetail from "./pages/BlogDetail";
+import FAQ from "./pages/FAQ";
+import Contact from "./pages/Contact";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<Offerings />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/offerings" element={<Offerings />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:slug" element={<BlogDetail />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
