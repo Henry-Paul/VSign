4cf96c1953e9d9bd5789f3fe6e74fad5268de27f
Henry-Paul <164559529+Henry-Paul@users.noreply.github.com>
2026-01-04 01:30:00 +0530

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as WhatsAppFloat } from "./WhatsAppFloat.jsx";
+export { default as PhoneFloat } from "./PhoneFloat.jsx";
+export { default as Header } from "./Header.jsx";
+export { default as Footer } from "./Footer.jsx";
+export { default as Layout } from "./Layout.jsx";
